d75117b2d5c7e1623ddc4f39356994cbfbbb78c8
Max Alva Garcia <alva85@gmail.com>
2023-02-01 07:39:15 -0500

    feat: 🎸 LogoutController

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,6 +49,11 @@
                     @enderror
                 </div>
 
+                <div class="mb-5">
+                    <input id="remember" type="checkbox" name="remember">
+                    <label for="remember" class="text-gray-500">Mantener mi sesión abierta</label>
+                </div>
+
                 <input type="submit" value="Iniciar Sesión"
                     class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
